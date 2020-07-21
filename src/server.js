b8ccc1dda1365e47d00c7bc4ef9facfbc77ac08f
OS <goloborodko@inbox.ru>
2020-07-21 15:18:37 +0300

    add express server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,23 @@
 require('@babel/register')();
 
+const express = require('express');
+const app = express();
+
 const render = require('./render').default;
 
-console.log(render());
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title></title>
+      </head>
+      <body>
+        <div id="root">${render()}</div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => console.log('Server is ready on 3000'));
